19a5a01b76ff56f92f0309d7019db594477f736a
Muzaffer Metehan Alan <mmalan@gtu.edu.tr>
2018-02-13 11:26:53 +0300

    Create mail.java

---
+++ src/main/java/com/metehan/thymeleaf/model/mail.java
@@ -0,0 +1,16 @@
+package com.metehan.thymeleaf.model;
+
+public class Mail {
+	private Person person;
+
+	public Person getPerson() {
+		return person;
+	}
+
+	public void setPerson(Person person) {
+		this.person = person;
+	}
+	
+	
+
+}
