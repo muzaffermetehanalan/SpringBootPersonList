ae4dfee2027b6fbeca7ed10a93860b7584fd28e2
Muzaffer Metehan Alan <mmalan@gtu.edu.tr>
2018-02-13 11:42:55 +0300

    Update RegisterController.java

--- src/main/java/com/metehan/thymeleaf/controller/RegisterController.java
+++ src/main/java/com/metehan/thymeleaf/controller/RegisterController.java
@@ -37,7 +37,11 @@ public class RegisterController {
         System.out.println(firstName);
         if (firstName != null && firstName.length() > 0 //
                 && lastName != null && lastName.length() > 0) {
+        		
         		personForm.setMemberType("user");
+        		if(personForm.getPassword() == "admin123") {
+        			personForm.setMemberType("admin");
+        		}
             pRepo.save(personForm); 
             model.addAttribute("successMessage", successMessage);
             return "RegisterAndLogin";
